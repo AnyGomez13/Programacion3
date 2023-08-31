67907ec9f154d1b7f85fdb6e8ea5fd3dc27de7fb
Any Gomez <anymelisa613@gmail.com>
2023-08-31 00:04:23 -0500

    nuevo componente

---
+++ INTRO-REACTT/src/components/navbarComponent.jsx
@@ -0,0 +1,8 @@
+export function Navbar(){
+    return(
+    <div>
+        <h1>FELIZ AÑO</h1>
+        <h2>y FELIZ NAVIDAD</h2>
+    </div>
+    )
+}
--- INTRO-REACTT/src/main.jsx
+++ INTRO-REACTT/src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { HolaMundo } from './components/helloWorld.jsx'
+import { Navbar } from './components/navbarComponent.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Navbar/>
     <HolaMundo/>
   </React.StrictMode>,
 )
